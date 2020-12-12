b8bae6009d16fc2d22ed2d58c7839dc24a6fa8f9
Benjamin Clos <benjamin.clos@gmail.com>
2020-12-12 14:13:06 -0700

    fix: handle old comments as noops;

--- .tasks/common/header.rb
+++ .tasks/common/header.rb
@@ -2,13 +2,19 @@ module Header
   HEADER_PATTERN = /^=begin\r?\n?(.+?)^=end/m
 
   def self.compile(file, out_dir)
-    basename = File.basename(file)
-    contents = File.read(file)
-    return :noop unless contents =~ HEADER_PATTERN
-    header = $1.strip
-    header_file = "%s.header" % basename.split(".").first
-    out_file = File.join(out_dir, header_file)
-    File.write(out_file, header)
-    header_file
+    begin
+      basename = File.basename(file)
+      contents = File.read(file)
+      return :noop unless contents =~ HEADER_PATTERN
+      header = $1.strip
+      header_file = "%s.header" % basename.split(".").first
+      out_file = File.join(out_dir, header_file)
+      File.write(out_file, header)
+      header_file
+    rescue => err
+      # silence really old garbage from comments
+      Log.out(err.message, label: file)
+      return :noop
+    end
   end
 end
